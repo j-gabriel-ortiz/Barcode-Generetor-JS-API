ad313c2b12ecf04d35e6b094b7d1c51a61bd7b5f
J Gabriel Ortiz <ojgabriel.88@gmail.com>
2024-05-08 18:06:14 -0300

    Actualizando el proyecto para tambien generar codigos QR

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const inputText = document.getElementById('convert-to-code');
-const btnGenerator = document.getElementById('btn-barcode-generator');
+const elementSVG = document.getElementById('barcode');
+const elementImg = document.getElementById('imgQR');
+const btnGeneratorBarcode = document.getElementById('btn-barcode-generator');
+const btnGeneratorQR = document.getElementById('btn-qr-generator');
 
-const barCode = () => {
-    JsBarcode('#barcode', inputText.value, {
-        format: "code128",
-        displayValue: true,
-        fontSize: 24,
-        lineColor: '#000'
-    });
+
+const urlQR = 'https://api.qrserver.com/v1/create-qr-code/?size=190x190&data=';
+
+const controlInput = () => {
+   return inputText.value !== "" ? true : alert('empty Field');
 }
 
-btnGenerator.addEventListener('click', barCode);
-inputText.addEventListener('keydown', (event) => {
-    if(event.key === 'Enter') {
-        barCode();
+const barCode = () => {
+    if(controlInput()) {
+        elementImg.classList.add('hidden');
+        elementSVG.classList.remove('hidden');
+        JsBarcode(elementSVG, inputText.value, {
+            format: "code128",
+            displayValue: true,
+            fontSize: 24,
+            lineColor: '#000'
+        });
     }
-} )
+}
+
+const codeQR = () => {
+    if(controlInput()) {
+        elementImg.src = `${urlQR}${inputText.value}`;
+        elementImg.classList.remove('hidden');
+        elementSVG.classList.add('hidden');
+    }
+}
+
+btnGeneratorBarcode.addEventListener('click', barCode);
+btnGeneratorQR.addEventListener('click', codeQR);
+
 
 window.onload = (event) => {
     inputText.value = '';
